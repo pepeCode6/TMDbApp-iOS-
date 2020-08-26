0a1a216bc7ef914a65104aa9f4b98403fce0dc4f
pepeMalpica <pepecode6@gmail.com>
2020-08-25 21:37:25 -0500

    Initial Commit

---
+++ TMDbApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TMDbApp
+//
+//  Created by pepe on 25/08/20.
+//  Copyright © 2020 pepeMalpica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
